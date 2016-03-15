1de2e7748012f83761d95e0d6ba7c5a5eba661d9
Jeff Eng <jeff@jeffeng.me>
2016-03-15 13:07:29 -0700

    factored out articleView.initIndexPage function calls within the fetchAll function.

--- class-07-functional-programming/pair-assignment/starter-code/scripts/article.js
+++ class-07-functional-programming/pair-assignment/starter-code/scripts/article.js
@@ -1,4 +1,4 @@
-// TODO: Wrap the entire contents of this file in an IIFE.
+// **DONE**: Wrap the entire contents of this file in an IIFE.
 // Pass in to the IIFE a module, upon which objects can be attached for later access.
 (function(module) {
   function Article (opts) {
@@ -41,20 +41,20 @@
   // This function will retrieve the data from either a local or remote source,
   // and process it, then hand off control to the View.
 
-  // TODO: Refactor this function, and provide it with a parameter of a callback function
+  // **DONE**: Refactor this function, and provide it with a parameter of a callback function
   //(for now just a placeholder, but to be referenced at call time as a view function)
   // to execute once the loading of articles is done. We do this because we might want
   // to call other view functions, and not just the initIndexPage() that we are replacing.
   // Now, instead of calling articleView.initIndexPage(), we can simply run our callback.
-  Article.fetchAll = function() {
+  Article.fetchAll = function(callbackFunction) {
     if (localStorage.rawData) {
       Article.loadAll(JSON.parse(localStorage.rawData));
-      articleView.initIndexPage();
+      callbackFunction();
     } else {
       $.getJSON('/data/hackerIpsum.json', function(rawData) {
         Article.loadAll(rawData);
         localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
-        articleView.initIndexPage();
+        callbackFunction();
       });
     }
   };
