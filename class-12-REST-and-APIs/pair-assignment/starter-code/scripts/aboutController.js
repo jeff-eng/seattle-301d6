b2ee70a7537c3e6469fcd34312cc6fcbc5aa595f
Mars <marshallmiller92@gmail.com>
2016-03-22 14:45:11 -0700

    set up callback

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
@@ -6,13 +6,8 @@
 
     // TODO: Call a function to load (AKA 'request') our repo data.
     // Pass in a view function as a callback, so our repos will render after the data is loaded.
-    $.ajax({ url: 'https://api.github.com/users/jeff-eng/repos' + '?per_page=5&sort=updated',
-        type: 'GET',
-        headers: {'Authorization': 'token ' + githubToken},
-        success: function (data, message, chr) {
-          console.log(data);
-        }
-      });
+
+    repos.requestRepos(repoView.index);
   };
 
   module.aboutController = aboutController;
